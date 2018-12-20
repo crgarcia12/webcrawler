e61776ce12a5b0f31e167d5eaf5c2b1312eabea4
Carlos Raul Garcia <crgar@microsoft.com>
2018-12-20 08:49:33 +0100

    function to download and upload images to blob

--- ArticleProcessor/index.js
+++ ArticleProcessor/index.js
@@ -1,3 +1,33 @@
+function UpoloadImageToBlob(imageUrl, blobName) {
+  // Download the image and upload it to the Blob Storage
+  var http = require('http');
+  http.get("http://avherald.com/img/france_a319_f-grhx_marseille_160627_1.jpg", function (httpResponse) {
+    if (200 !== httpResponse.statusCode) {
+      context.log('Unexpected status code: %d', httpResponse.statusCode);
+    } else {
+        var writeStream = blobService.createWriteStreamToBlockBlob(
+            containerName,
+            "foto.jpg",
+            {
+                contentSettings: {
+                    contentType: 'text/html'
+                }
+            },
+            function(error, result, response){
+                if(error){
+                    context.log("Couldn't upload file %s from %s", fileName, domain);
+                    context.error(error);
+                } else {
+                    context.log('File %s from %s uploaded', fileName, domain);
+                }
+            });
+        httpResponse.pipe(writeStream);
+    }
+  }).on('error', function(e) {
+    context.log("Got error: " + e.message);
+  });
+}
+
 module.exports = async function (context, myQueueItem) {
     context.log('JavaScript queue trigger function processed work item', myQueueItem);
 
@@ -7,9 +37,11 @@ module.exports = async function (context, myQueueItem) {
         path: '/index.html'
     };
       
+    // Getting the article nr that we should process
     context.log('JavaScript queue trigger function processed work item', myQueueItem);
-    const url = 'http://avherald.com/h?article=4c1c52bc&opt=0';
+    const url = `http://avherald.com/h?article=${myQueueItem}&opt=0`;
 
+    // Getting the HTTP source code
     const puppeteer = require('puppeteer');
     
     puppeteer
@@ -27,5 +59,22 @@ module.exports = async function (context, myQueueItem) {
       })
       .catch(function(err) {
         context.log(err)
+    });
+
+    // Downloading images and send them to the blob
+    var azure = require('azure-storage');
+    var imagesAccountName = process.env["imagesAccountName"];
+    var imagesAccountKey = process.env["imagesAccountKey"];
+    var containerName = 'images';
+
+    // Create the container
+    var blobService = azure.createBlobService(imagesAccountName, imagesAccountKey);
+    blobService.createContainerIfNotExists(containerName, {
+        publicAccessLevel: 'blob'
+      }, function(error, result, response) {
+        if (!error) {
+          // if result = true, container was created.
+          // if result = false, container already existed.
+        }
     });
 };
