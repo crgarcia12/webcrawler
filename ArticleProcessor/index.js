4d46207b97cbd0bcbc2d29285806c35fbeedd039
Carlos Raul Garcia <crgar@microsoft.com>
2018-12-20 10:09:05 +0100

    E2E flow :)

--- ArticleProcessor/index.js
+++ ArticleProcessor/index.js
@@ -1,13 +1,15 @@
-function UpoloadImageToBlob(imageUrl, blobName) {
+function UpoloadImageToBlob(imageUrl, containerName, blobName) {
+  var blobService = CreateBlobService(containerName);
+
   // Download the image and upload it to the Blob Storage
   var http = require('http');
-  http.get("http://avherald.com/img/france_a319_f-grhx_marseille_160627_1.jpg", function (httpResponse) {
+  http.get(imageUrl, function (httpResponse) {
     if (200 !== httpResponse.statusCode) {
       context.log('Unexpected status code: %d', httpResponse.statusCode);
     } else {
         var writeStream = blobService.createWriteStreamToBlockBlob(
             containerName,
-            "foto.jpg",
+            blobName,
             {
                 contentSettings: {
                     contentType: 'text/html'
@@ -15,10 +17,10 @@ function UpoloadImageToBlob(imageUrl, blobName) {
             },
             function(error, result, response){
                 if(error){
-                    context.log("Couldn't upload file %s from %s", fileName, domain);
+                    context.log("Couldn't upload file %s from %s", blobName, imageUrl);
                     context.error(error);
                 } else {
-                    context.log('File %s from %s uploaded', fileName, domain);
+                    context.log('File %s from %s uploaded', blobName, imageUrl);
                 }
             });
         httpResponse.pipe(writeStream);
@@ -28,53 +30,69 @@ function UpoloadImageToBlob(imageUrl, blobName) {
   });
 }
 
-module.exports = async function (context, myQueueItem) {
-    context.log('JavaScript queue trigger function processed work item', myQueueItem);
+function DownloadWebPageProcessedSourceCode(url) {
+  // Getting the HTTP source code
+  const puppeteer = require('puppeteer');
+    
+  return puppeteer
+    .launch()
+    .then(function(browser) {
+      return browser.newPage();
+    })
+    .then(function(page) {
+      return page.goto(url).then(function() {
+        return page.content();
+      });
+    })
+    .then(function(html) {
+      return html;
+    })
+    .catch(function(err) {
+      context.log(err)
+  });
+}
 
-    var options = {
-        host: 'www.google.com',
-        port: 80,
-        path: '/index.html'
-    };
-      
+function CreateBlobService (containerName){
+  var azure = require('azure-storage');
+  var imagesAccountName = process.env["imagesAccountName"];
+  var imagesAccountKey = process.env["imagesAccountKey"];
+
+
+  // Create the container if it does not exist
+  var blobService = azure.createBlobService(imagesAccountName, imagesAccountKey);
+  blobService.createContainerIfNotExists(containerName, {
+      publicAccessLevel: 'blob'
+    }, function(error, result, response) {
+      if (!error) {
+        // if result = true, container was created.
+        // if result = false, container already existed.
+      }
+  });
+  return blobService;
+}
+
+module.exports = async function (context, myQueueItem) {
+    var containerName = 'images';
+
     // Getting the article nr that we should process
     context.log('JavaScript queue trigger function processed work item', myQueueItem);
     const url = `http://avherald.com/h?article=${myQueueItem}&opt=0`;
 
-    // Getting the HTTP source code
-    const puppeteer = require('puppeteer');
-    
-    puppeteer
-      .launch()
-      .then(function(browser) {
-        return browser.newPage();
-      })
-      .then(function(page) {
-        return page.goto(url).then(function() {
-          return page.content();
-        });
-      })
-      .then(function(html) {
-        context.log(html);
-      })
-      .catch(function(err) {
-        context.log(err)
+    // Download HTML code and execute all the JS on it. We need a full DOM
+    var htmlCode = DownloadWebPageProcessedSourceCode(url)
+    .then(function(htmlContent) {
+      var imageUrls = htmlContent.match('http://avherald.com\\/img\\/[\\w-_.]+');
+      context.log(imageUrls);
+      return imageUrls;
+    }).then(function(imageUrls) {
+      imageUrls.forEach(url => {
+        UpoloadImageToBlob(url, containerName, "foto2.jpg");
+      });
     });
 
-    // Downloading images and send them to the blob
-    var azure = require('azure-storage');
-    var imagesAccountName = process.env["imagesAccountName"];
-    var imagesAccountKey = process.env["imagesAccountKey"];
-    var containerName = 'images';
+    // Create a blob service and make sure the container is in place
+    //
 
-    // Create the container
-    var blobService = azure.createBlobService(imagesAccountName, imagesAccountKey);
-    blobService.createContainerIfNotExists(containerName, {
-        publicAccessLevel: 'blob'
-      }, function(error, result, response) {
-        if (!error) {
-          // if result = true, container was created.
-          // if result = false, container already existed.
-        }
-    });
+
+
 };
